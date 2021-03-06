1baf985e2faadfed668ccccd01b9cea4de34af9d
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2021-03-06 08:32:28 +0100

    Adding sections of type Organisation to Organisation.

---
+++ TimeKeeper.Data/Migrations/20210306073121_Adding_sections.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeKeeper.Data.Migrations
+{
+    public partial class Adding_sections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrganisationId",
+                table: "Organisation",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Organisation_OrganisationId",
+                table: "Organisation",
+                column: "OrganisationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Organisation_Organisation_OrganisationId",
+                table: "Organisation",
+                column: "OrganisationId",
+                principalTable: "Organisation",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Organisation_Organisation_OrganisationId",
+                table: "Organisation");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Organisation_OrganisationId",
+                table: "Organisation");
+
+            migrationBuilder.DropColumn(
+                name: "OrganisationId",
+                table: "Organisation");
+        }
+    }
+}
--- TimeKeeper.Data/Models/Organisation.cs
+++ TimeKeeper.Data/Models/Organisation.cs
@@ -1,9 +1,12 @@
 ﻿
+using System.Collections.Generic;
+
 namespace TimeKeeper.Data.Models
 {
     public class Organisation
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Organisation> Section { get; set; }
     }
 }
